36bfc2a440472d8e8fa419499498e54923218739
antonioferris <antonioferris01@gmail.com>
2019-11-14 04:06:12 -0800

    Added extraction of json

--- data.py
+++ data.py
@@ -2,3 +2,21 @@
     This file will contain helper functions used to extract useful pythonic structures
     out of the json training and test data provided by Clickbait Challenge.
 """
+import json, os
+
+
+# Given a pathname,
+# Returns instance and truth, both scraped from the json
+def getRawData(pathname):
+    instance_path = os.path.join('.', pathname, "instances.jsonl")
+    truth_path = os.path.join('.', pathname, "truth.jsonl")
+    instance = []
+    truth = []
+    with open(truth_path, "r") as truth_file:
+        for line in truth_file:
+            truth.append(json.loads(line))
+    with open(instance_path, "r", encoding='utf-8') as instance_file:
+        for line in instance_file:
+            instance.append(json.loads(line))
+    # print('BABAGANOUSH') Wonder if Johannes will find this...
+    return (instance, truth)
--- model.py
+++ model.py
@@ -2,4 +2,8 @@
     This file will contain the various models that we will employ, along with helper
     functions to test these models.
 """
+import data
 
+def dumbClassifier(pathname):
+    instance, truth = data.getRawData(pathname)
+    
