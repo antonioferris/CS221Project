72875b697a5ac30c3ebc6f17d4da8ee25b3ac67b
Johannes Hui <huijohannes@gmail.com>
2019-11-14 20:44:50 -0800

    edited model

--- model.py
+++ model.py
@@ -7,11 +7,13 @@ import eval, subprocess
 import util
 import nltk
 nltk.download('stopwords')
+import collections
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
 
-print (set(stopwords.words('english')))
 # This classifier will say something is clickbait if it has ! or ? in the title
 def dumbClassifier():
     return lambda inst : 1 if set(inst["targetTitle"]) & set("?!") else 0
@@ -32,19 +34,52 @@ def testClassifier(func, name='untitled.testoutput'):
     subprocess.run(["python", "eval.py", util.VAL_TRUTH_PATH, '.tmpdmp', name])
 
 def classifier(inst):
+    stopwords = set(stopwords.words('english'))
     title = inst["targetTitle"]
     description = inst["targetDescription"]
     keywords = inst["targetKeywords"]
     paragraphs = inst["targetParagraphs"]
 
+    #Process text
+    def processText(text):
+        tokens = word_tokenize(text)
+        lower = [word.lower() for word in tokens if word.isalpha()]
+        stop_tokens = [word in lower if word not in stopwords]
+        return stop_tokens
+    
+    #Gets punctuation counts in title
+    def countPunc(text):
+        processedText = processText(text)
+        punct_count = collections.defaultdict(int)
+        for word in processedText:
+            if word in string.punctuation:
+                punct_count[word] += 1
+                total_count += 1
+        return punct_count, total_count
+
     #Feature: Title punctuation
+    title_punc, title_punc_count = countPunc(title)
+
+    #Feature: count of !
+    title_exclam_count = title_punc["!"]
+
+    #Feature: count of ?
+    title_question_count = title_punch['?']
 
     #Feature: Number of stopwords in title
     def title_stopwords_feature:
+        count = 0
         for word in title:
-            if 
+            if word in stopwords:
+                count += 1
+        return count
 
     #Feature: Average word length
+    def avg_word_len(text):
+        total_len = 0
+        for word in text:
+            total_len += len(word)
+        return total_len//len(text)
 
     #Feature: Paragraph length
 
@@ -54,6 +89,19 @@ def classifier(inst):
 
     #Feature: Number Named Entities
 
+    #Proper Nouns in Title
+    tagged_title = pos_tag(title)
+    title_proper_nouns = [word for word, pos in tagged_title if pos == 'NNP']
+
+    #Proper Nouns in Keywords
+    tagged_keywords = pos_tag(keywords)
+    keywords_proper_nouns = [word for word, pos in tagged_keywords if pos == 'NNP']
+
+    #Feature: Title Word2Vec
+    processed_title = processText(title)
+
+    #Feature: Keyword Word2Vec
+    processed_keywords = processText(keyword)
 
 
 
