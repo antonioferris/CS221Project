50f97c813f401be763e63a133b4365b94f85e904
Johannes Hui <huijohannes@gmail.com>
2019-11-14 19:50:54 -0800

    Edited model

--- model.py
+++ model.py
@@ -5,7 +5,13 @@
 import data, main
 import eval, subprocess
 import util
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
 
+print (set(stopwords.words('english')))
 # This classifier will say something is clickbait if it has ! or ? in the title
 def dumbClassifier():
     return lambda inst : 1 if set(inst["targetTitle"]) & set("?!") else 0
@@ -24,3 +30,25 @@ def testClassifier(func, name='untitled.testoutput'):
         results[_id] = str(func(inst))
     util.dumpResults(results, '.tmpdmp')
     subprocess.run(["python", "eval.py", util.VAL_TRUTH_PATH, '.tmpdmp', name])
+
+def classifier(inst):
+    title = inst["targetTitle"]
+    description = inst["targetDescription"]
+    keywords = inst["targetKeywords"]
+    paragraphs = inst["targetParagraphs"]
+
+    #Feature: Title punctuation
+
+    #Feature: Average word length
+
+    #Feature: Paragraph length
+
+    #Feature: Number of paragraphs
+
+    #Feature: Number of keywords
+
+    #Feature: Number Named Entities
+
+
+
+
