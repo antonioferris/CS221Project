8404c770a3b7b70c1b4fefc1893665468250c09c
antonioferris <antonioferris01@gmail.com>
2019-12-01 01:37:20 -0800

    Repickled data

--- data.py
+++ data.py
@@ -22,8 +22,79 @@ def getTrainValTest():
 
 def getPickledData(pathname):
     with open(pathname, 'rb') as f:
-        labels, D = pickle.load(f)
-    return labels, D
+        D = pickle.load(f)
+    return D
+
+def pickleData():
+    train_truth_path = 'clickbait17-train-170331\\truth.jsonl'
+    train_inst_path = 'clickbait17-train-170331\instances.jsonl'
+    val_truth_path = 'clickbait17-train-170331\\truth.jsonl'
+    val_inst_path = 'clickbait17-train-170331\instances.jsonl'
+    raw_tr_inst = []
+    raw_tr_truth = []
+    raw_val_inst = []
+    raw_val_truth = []
+    with open(train_inst_path, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            raw_tr_inst.append(json.loads(line))
+    with open(train_truth_path, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            raw_tr_truth.append(json.loads(line))
+    with open(val_inst_path, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            raw_val_inst.append(json.loads(line))
+    with open(val_truth_path, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            raw_val_truth.append(json.loads(line))
+    n1 = len(raw_tr_inst)
+    assert(len(raw_tr_inst) == len(raw_tr_truth))
+    n2 = len(raw_val_inst)
+    assert(len(raw_val_inst) == len(raw_val_truth))
+
+    to_pickle_train = []
+    to_pickle_val = []
+    for i in range(n1):
+        row = []
+        inst = raw_tr_inst[i]
+        for j in range(n1):
+            if raw_tr_inst[i]["id"] == raw_tr_truth[j]["id"]:
+                truth = raw_tr_truth[j]
+                break
+        for key in util.labels:
+            if key in inst:
+                row.append(inst[key])
+            else:
+                row.append(truth[key])
+        to_pickle_train.append(row)
+    s1 = set()
+    for i in range(n1):
+        assert(len(to_pickle_train[i]) == len(util.labels))
+        s1.add(to_pickle_train[i][util.label_dict["id"]])
+    assert(len(s1) == n1)
+
+    for i in range(n2):
+        row = []
+        inst = raw_val_inst[i]
+        for j in range(n2):
+            if raw_val_inst[i]["id"] == raw_val_truth[j]["id"]:
+                truth = raw_val_truth[j]
+                break
+        for key in util.labels:
+            if key in inst:
+                row.append(inst[key])
+            else:
+                row.append(truth[key])
+        to_pickle_val.append(row)
+    s2 = set()
+    for i in range(n2):
+        assert(len(to_pickle_val[i]) == len(util.labels))
+        s2.add(to_pickle_val[i][util.label_dict["id"]])
+    assert(len(s2) == n2)
+
+    pickle.dump(to_pickle_train, open('train_data.pickle', 'wb'))
+    pickle.dump(to_pickle_val, open('val_data.pickle', 'wb'))
+        
+
 
 # Given a pathname,
 # train or val simply tell getRawData to use a pre-assigned path
--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def main():
     print('Finished Startup')   
     train_data, cross_val_data, test_data, labels = data.getTrainValTest()
 
+    data.pickleData()
+
     # dumbc = model.DumbClassifier()
     # print('Dumb Classifier (clickbait if ! or ? in the title')
     # dumbc.test(cross_val_data)
@@ -20,9 +22,9 @@ def main():
     # print('First Model (num punctuation features)')
     # fm.test(cross_val_data)
 
-    dvm = doc2vecmodel.Doc2VecModel(train_data)
-    print('Doc 2 Vec Model')
-    dvm.test(cross_val_data, threshold=0.2)
+    # dvm = doc2vecmodel.Doc2VecModel(train_data, makeModel=True)
+    # print('Doc 2 Vec Model')
+    # dvm.test(cross_val_data, threshold=0.2)
 
     # mcm = multiclassmodel.MultiClassModel(train_data);
     # print('MultiClass Model')
